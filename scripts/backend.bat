bf74b4bf580b7f2f2cd3f924f4d5d621ac2ba657
xBOJox <lazarbojovic@gmail.com>
2022-03-03 16:03:52 +0100

    windows runserver

--- scripts/backend.bat
+++ scripts/backend.bat
@@ -1,12 +1,6 @@
 @echo off
 
-call .venv/Scripts/activate.bat
-
 cd django-projekat
 
-if %1 == "-h" (
-  call "python manage.py runserver 0.0.0.0:8000"
-) else (
-  call "python manage.py runserver"
-)
-
+call .venv/Scripts/activate.bat
+if %1 == -h (call python manage.py runserver 0.0.0.0:8000) else (call python manage.py runserver)
