3ed122ed0b8ab1e85ac30f7dc164e2c0d93727b7
xBOJox <lazarbojovic@gmail.com>
2022-03-05 13:01:20 +0100

    end-screen-css

--- trafficsigns-vue/src/views/EndView.vue
+++ trafficsigns-vue/src/views/EndView.vue
@@ -25,7 +25,9 @@ const store = useStore();
           <p class="netacni">{{ store.brNetacnih }}</p>
         </div>
       </div>
-      <NButton @click="store.$reset()" class="restart-button">Restart</NButton>
+      <NButton @click="store.$reset()" class="restart-button"
+        ><h1>Restart</h1></NButton
+      >
     </div>
   </div>
 </template>
@@ -36,22 +38,30 @@ const store = useStore();
   text-align: center;
   margin-top: 5rem;
 }
-.end-text {
+.end-screen {
   background-color: rgba(255, 255, 255, 0.9);
+  padding: 20px;
+  border-radius: 18px;
+}
+.end-text {
   color: black;
   display: inline-block;
   font-weight: bold;
   font-size: larger;
   margin: auto;
 }
-.end-stats{
-  background-color: rgba(255, 255, 255, 0.9);
+.end-stats {
   color: black;
 }
 .restart-button {
-  background: rgb(212,0,0);
-  background: linear-gradient(90deg, rgba(212,0,0,1) 10%, rgba(203,24,18,0.1600841020001751) 56%, rgba(0,26,255,1) 100%);
-  border:2px solid #ffffff;
+  background: rgb(212, 0, 0);
+  background: linear-gradient(
+    90deg,
+    rgba(212, 0, 0, 1) 10%,
+    rgba(203, 24, 18, 0.5600841020001751) 50%,
+    rgba(0, 26, 255, 1) 100%
+  );
+  border: 2px solid #ffffff;
   color: white;
   border-radius: 10px;
   margin-top: 2rem;
